471951e35b84c7349775b961987c7334c96172a0
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-26 23:10:42 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,11 @@
 class Owner
-  # code goes here
+  
   attr_accessor :fishes, :cats, :dogs
   fishes = []
   cats = []
   dogs = []
-
+  
   def initialize(pets)
-    @pets = pets
+    @pets = {fishes: [], cats:[], dogs: []}
   end
 end
